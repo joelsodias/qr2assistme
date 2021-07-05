2df03440c543ca3b94aad7e71c6dd6e689572dd0
Joelso Dias <joelsodias@gmail.com>
2021-07-05 01:20:37 -0300

    Facebook Login Integration

--- app/Views/content/chat/chat_attendee_login_view.php
+++ app/Views/content/chat/chat_attendee_login_view.php
@@ -30,7 +30,12 @@
 
 <?= $this->section('content') ?>
 <section class="h-100 w-100 border d-flex justify-content-center align-items-center">
-
+<!-- <div
+  class="fb-like"
+  data-share="true"
+  data-width="450"
+  data-show-faces="true">
+</div> -->
     <div class="">
     <header class="text-center">
         <img class="logo" src="/images/logo.png">
@@ -44,10 +49,10 @@
         <div class="row m-3">
             <div class="col-12 d-flex justify-content-center align-items-center flex-wrap">
                 <a href="<?= $google["auth_url"] ?? "#error" ?>" class="btn btn-danger  m-3"><i class="fab fa-google-plus-square fa-3x"></i></a>
-                <a href="/attendee/chat/guest" class="btn btn-dark m-3"><i class="fab fa-facebook-square fa-3x"></i></a>
-                <a href="/attendee/chat/guest" class="btn btn-dark m-3"><i class="fab fa-linkedin fa-3x"></i></a>
+                <a href="<?= $facebook["auth_url"] ?? "#error" ?>" class="btn btn-primary m-3"><i class="fab fa-facebook-square fa-3x"></i></a>
+                <!-- <a href="/attendee/chat/guest" class="btn btn-dark m-3"><i class="fab fa-linkedin fa-3x"></i></a>
                 <a href="/attendee/chat/guest" class="btn btn-dark m-3"><i class="fab fa-microsoft fa-3x"></i></i></a>
-                <a href="/attendee/chat/guest" class="btn btn-dark m-3"><i class="fab fa-instagram fa-3x"></i></i></a>
+                <a href="/attendee/chat/guest" class="btn btn-dark m-3"><i class="fab fa-instagram fa-3x"></i></i></a> -->
             </div>
         </div>
 
--- app/Views/layouts/layout_bootstrap_clear_noresize.php
+++ app/Views/layouts/layout_bootstrap_clear_noresize.php
@@ -20,6 +20,24 @@
 </head>
 
 <body>
+<script>
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '291594885908864',
+      xfbml      : true,
+      version    : 'v11.0'
+    });
+    FB.AppEvents.logPageView();
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "https://connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+</script>
     <main>
         <?= $this->renderSection("content") ?>
     </main>

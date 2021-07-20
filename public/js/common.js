3ed8244e6617965379734335e00e1aee7e15b36e
Joelso Dias <joelsodias@gmail.com>
2021-07-20 09:34:29 -0300

    Check if "moment" is attached to current template

--- public/js/common.js
+++ public/js/common.js
@@ -180,56 +180,57 @@ $(function() {
 // moment.js locale configuration
 // locale : brazilian portuguese (pt-br)
 // author : Caio Ribeiro Pereira : https://github.com/caio-ribeiro-pereira
-
-$(function(factory) {
-    if (typeof define === 'function' && define.amd) {
-        define(['moment'], factory); // AMD
-    } else if (typeof exports === 'object') {
-        module.exports = factory(require('../moment')); // Node
-    } else {
-        factory(window.moment); // Browser global
-    }
-}(function(moment) {
-    return moment.defineLocale('pt-br', {
-        months: 'janeiro_fevereiro_março_abril_maio_junho_julho_agosto_setembro_outubro_novembro_dezembro'.split('_'),
-        monthsShort: 'Jan_Fev_Mar_Abr_Mai_Jun_Jul_Ago_Set_Out_Nov_Dez'.split('_'),
-        weekdays: 'domingo_segunda-feira_terça-feira_quarta-feira_quinta-feira_sexta-feira_sábado'.split('_'),
-        weekdaysShort: 'dom_seg_ter_qua_qui_sex_sáb'.split('_'),
-        weekdaysMin: 'dom_2ª_3ª_4ª_5ª_6ª_sáb'.split('_'),
-        longDateFormat: {
-            LT: 'HH:mm',
-            L: 'DD/MM/YYYY',
-            LL: 'D [de] MMMM [de] YYYY',
-            LLL: 'D [de] MMMM [de] YYYY [às] LT',
-            LLLL: 'dddd, D [de] MMMM [de] YYYY [às] LT'
-        },
-        calendar: {
-            sameDay: '[Hoje às] LT',
-            nextDay: '[Amanhã às] LT',
-            nextWeek: 'dddd [às] LT',
-            lastDay: '[Ontem às] LT',
-            lastWeek: function() {
-                return (this.day() === 0 || this.day() === 6) ?
-                    '[Último] dddd [às] LT' : // Saturday + Sunday
-                    '[Última] dddd [às] LT'; // Monday - Friday
+if (window.moment) {
+    $(function(factory) {
+        if (typeof define === 'function' && define.amd) {
+            define(['moment'], factory); // AMD
+        } else if (typeof exports === 'object') {
+            module.exports = factory(require('../moment')); // Node
+        } else {
+            factory(window.moment); // Browser global
+        }
+    }(function(moment) {
+        return moment.defineLocale('pt-br', {
+            months: 'janeiro_fevereiro_março_abril_maio_junho_julho_agosto_setembro_outubro_novembro_dezembro'.split('_'),
+            monthsShort: 'Jan_Fev_Mar_Abr_Mai_Jun_Jul_Ago_Set_Out_Nov_Dez'.split('_'),
+            weekdays: 'domingo_segunda-feira_terça-feira_quarta-feira_quinta-feira_sexta-feira_sábado'.split('_'),
+            weekdaysShort: 'dom_seg_ter_qua_qui_sex_sáb'.split('_'),
+            weekdaysMin: 'dom_2ª_3ª_4ª_5ª_6ª_sáb'.split('_'),
+            longDateFormat: {
+                LT: 'HH:mm',
+                L: 'DD/MM/YYYY',
+                LL: 'D [de] MMMM [de] YYYY',
+                LLL: 'D [de] MMMM [de] YYYY [às] LT',
+                LLLL: 'dddd, D [de] MMMM [de] YYYY [às] LT'
             },
-            sameElse: 'L'
-        },
-        relativeTime: {
-            future: 'em %s',
-            past: '%s atrás',
-            s: 'segundos',
-            m: 'um minuto',
-            mm: '%d minutos',
-            h: 'uma hora',
-            hh: '%d horas',
-            d: 'um dia',
-            dd: '%d dias',
-            M: 'um mês',
-            MM: '%d meses',
-            y: 'um ano',
-            yy: '%d anos'
-        },
-        ordinal: '%dº'
-    });
-}));
+            calendar: {
+                sameDay: '[Hoje às] LT',
+                nextDay: '[Amanhã às] LT',
+                nextWeek: 'dddd [às] LT',
+                lastDay: '[Ontem às] LT',
+                lastWeek: function() {
+                    return (this.day() === 0 || this.day() === 6) ?
+                        '[Último] dddd [às] LT' : // Saturday + Sunday
+                        '[Última] dddd [às] LT'; // Monday - Friday
+                },
+                sameElse: 'L'
+            },
+            relativeTime: {
+                future: 'em %s',
+                past: '%s atrás',
+                s: 'segundos',
+                m: 'um minuto',
+                mm: '%d minutos',
+                h: 'uma hora',
+                hh: '%d horas',
+                d: 'um dia',
+                dd: '%d dias',
+                M: 'um mês',
+                MM: '%d meses',
+                y: 'um ano',
+                yy: '%d anos'
+            },
+            ordinal: '%dº'
+        });
+    }));
+}
